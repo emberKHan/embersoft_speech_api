1f15b4bdc79b02a50b9274e4b1ea9d37c295eed1
emberKHan <96856982+emberKHan@users.noreply.github.com>
2022-02-22 16:30:35 +0800

    Update sender.py

--- sender.py
+++ sender.py
@@ -3,8 +3,8 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('audio', type=str, help='Input audio path')
-parser.add_argument('--mode', type=str, help='send mode', default='force')
-parser.add_argument('--url', type=str, default='http://34.92.222.84/')
+parser.add_argument('--mode', type=str, help='send mode', default='raw')
+parser.add_argument('--url', type=str, default='http://202.131.230.114:1020/')
 args = parser.parse_args()
 
 def main(args):
